0ac587dcddcec0073d0e9c38d3673853dec80080
yaman3bd <yaman3bd@gmail.com>
2025-12-04 03:03:11 +0300

    chore: update wishlist script

--- assets/js/wishlist.js
+++ assets/js/wishlist.js
@@ -14,6 +14,13 @@
 
 class WishlistManager {
   constructor() {
+    // Translatable strings (can be overridden via window.wishlistConfig)
+    this.LABELS = {
+      addToWishlist: window.wishlistConfig?.labels?.addToWishlist || "Add to wishlist",
+      removeFromWishlist: window.wishlistConfig?.labels?.removeFromWishlist || "Remove from wishlist",
+      loading: window.wishlistConfig?.labels?.loading || "Loading"
+    };
+
     // SVG Icons as constants
     this.ICONS = {
       EMPTY_HEART: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary" fill="none" > <path d="M12.001 4.52853C14.35 2.42 17.98 2.49 20.2426 4.75736C22.5053 7.02472 22.583 10.637 20.4786 12.993L11.9999 21.485L3.52138 12.993C1.41705 10.637 1.49571 7.01901 3.75736 4.75736C6.02157 2.49315 9.64519 2.41687 12.001 4.52853ZM18.827 6.1701C17.3279 4.66794 14.9076 4.60701 13.337 6.01687L12.0019 7.21524L10.6661 6.01781C9.09098 4.60597 6.67506 4.66808 5.17157 6.17157C3.68183 7.66131 3.60704 10.0473 4.97993 11.6232L11.9999 18.6543L19.0201 11.6232C20.3935 10.0467 20.319 7.66525 18.827 6.1701Z" fill="currentColor" ></path> </svg>`,
@@ -86,6 +93,9 @@ class WishlistManager {
 
     // Setup event click all wishlist buttons
     document.addEventListener("click", this.handleWishlistClick);
+
+    // Subscribe to products-updated event (AJAX filtering)
+    window.addEventListener("products-updated", () => this.updateAllButtons());
 
     this.isInitialized = true;
   }
@@ -242,28 +252,28 @@ class WishlistManager {
       case "guest":
         // Guest user - show empty heart
         button.innerHTML = this.ICONS.EMPTY_HEART;
-        button.setAttribute("aria-label", '{{ _("Add to wishlist") }}');
+        button.setAttribute("aria-label", this.LABELS.addToWishlist);
         button.disabled = false;
         break;
 
       case "empty":
         // Logged-in, not in wishlist - show empty heart
         button.innerHTML = this.ICONS.EMPTY_HEART;
-        button.setAttribute("aria-label", '{{ _("Add to wishlist") }}');
+        button.setAttribute("aria-label", this.LABELS.addToWishlist);
         button.disabled = false;
         break;
 
       case "filled":
         // In wishlist - show filled heart in red
         button.innerHTML = this.ICONS.FILLED_HEART;
-        button.setAttribute("aria-label", '{{ _("Remove from wishlist") }}');
+        button.setAttribute("aria-label", this.LABELS.removeFromWishlist);
         button.disabled = false;
         break;
 
       case "loading":
         // Loading state - show spinner
         button.innerHTML = this.ICONS.SPINNER;
-        button.setAttribute("aria-label", '{{ _("Loading") }}');
+        button.setAttribute("aria-label", this.LABELS.loading);
         button.disabled = true;
         break;
     }
